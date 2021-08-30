e06fb7b85f669f079f1915e190f35358dddf0dcb
Andrew <emtsevaa@mail.ru>
2021-08-30 11:35:54 +0300

    проба первого класса

---
+++ src/Main.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace Generator;
+
+
+class Main
+{
+    /**
+     * @var string
+     */
+    private $name;
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+}
